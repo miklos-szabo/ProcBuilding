92f0d030ef8af36095dfbc6ebe4824bce0a34616
Szabó Miklós <szabo.miklos@ma-coding.com>
2021-02-14 22:17:16 +0100

    Width spacing on ends

--- Assets/BuildingGenerator.cs
+++ Assets/BuildingGenerator.cs
@@ -1,25 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using UnityEditor.ProBuilder;
 using UnityEngine;
 using UnityEngine.ProBuilder;
 using UnityEngine.ProBuilder.MeshOperations;
+using Math = System.Math;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+using Vector4 = UnityEngine.Vector4;
 
 public class BuildingGenerator : MonoBehaviour
 {
     [SerializeField] private Vector3 genCoords = Vector3.zero;
-    [SerializeField] private float height = 30f;
-    [SerializeField] private float width = 20f;
+    
+    [SerializeField] private float heightMax = 100f;
+    [SerializeField] private float heigthMin = 10f;
+    [SerializeField] private float widthMax = 100f;
+    [SerializeField] private float widthMin = 10f;
+    
     [SerializeField] private Material wallMaterial;
     [SerializeField] private Material windowMaterial;
     [SerializeField] private Material doorMaterial;
+    
     [SerializeField] private float windowHeight = 2f;
     [SerializeField] private float windowWidth = 2f;
     [SerializeField] [Tooltip("Két ablak közötti távolság fele vízszintesen")] private float windowWidthSpacing = 1.5f;
     [SerializeField] [Tooltip("Két ablak közötti távolság fele függőlegesen")] private float windowHeightSpacing = .5f;
     [SerializeField] private float doorSectionHeight = 4f;
+    [SerializeField] private float doorHeight = 3f;
+    [SerializeField] private float doorWidth = 2.5f;
 
+    private float height = 30f;
+    private float width = 20f;
+
+    private int buildingsGenerated = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -35,23 +52,31 @@ public class BuildingGenerator : MonoBehaviour
 
     private void GenerateBuilding()
     {
+        //Randomize height and width
+        height = Random.Range(heigthMin, heightMax);
+        width = Random.Range(widthMin, widthMax);
+
         var mainVertices = new Vector3[]
         {
-            new Vector3(0f, 0f, 0f),
-            new Vector3(width, 0f, 0f),
-            new Vector3(0f, height, 0f),
-            new Vector3(width, height, 0f)
+            genCoords,
+            genCoords + new Vector3(width, 0f),
+            genCoords + new Vector3(0f, height),
+            genCoords + new Vector3(width, height)
         };
         var mainFace = new Face(new int[] {1, 0, 3, 0, 2, 3});
         var faces = new List<Face>{mainFace};
+
+        var doorFace = CreateDoor(ref mainVertices);
+        faces.Add(doorFace);
 
         var windowFaces = CreateWindows(ref mainVertices);
         faces.AddRange(windowFaces);
         
         var mesh = ProBuilderMesh.Create(mainVertices, faces);
         
-        mesh.GetComponent<MeshRenderer>().sharedMaterials = new[] {wallMaterial, windowMaterial};
+        mesh.GetComponent<MeshRenderer>().sharedMaterials = new[] {wallMaterial, windowMaterial, doorMaterial};
         mainFace.submeshIndex = 0;
+        doorFace.submeshIndex = 2;
         
         var uvs = new List<Vector4>();
         mesh.GetUVs(0, uvs);
@@ -65,7 +90,7 @@ public class BuildingGenerator : MonoBehaviour
         // uvs[mainFaceIndexes[3]] = new Vector4(0.82f, 1f);
         mainFace.uv = new AutoUnwrapSettings
         {
-            scale = new Vector2(15f, 15f)
+            scale = new Vector2(0.1f, 0.1f)
         };
 
         //Set UV for all window faces
@@ -85,9 +110,40 @@ public class BuildingGenerator : MonoBehaviour
         
         //Materials overlap, extrude the windows a bit
         mesh.Extrude(windowFaces, ExtrudeMethod.FaceNormal, 0.01f);
+        mesh.Extrude(new List<Face>{doorFace}, ExtrudeMethod.FaceNormal, 0.01f);
         
         mesh.ToMesh();
         mesh.Refresh();
+
+        buildingsGenerated++;
+        genCoords += new Vector3(0, 0, -30);
+
+        //Move camera backwards to see the building
+        var camera = FindObjectOfType<Camera>();
+        camera.transform.position += new Vector3(0, 0, -30);
+    }
+
+    private void RandomizeMeasurements()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private Face CreateDoor(ref Vector3[] vertices)
+    {
+        //Door will be between 25% and 75% of width
+        var leftCoord = Random.Range(width - width * 0.75f, width - doorWidth - width * 0.25f);
+        var lowerLeft = new Vector3(leftCoord, 0, genCoords.z);
+        
+        var vertCount = vertices.Length;
+        var newVertices = new Vector3[]
+        {
+            lowerLeft,
+            lowerLeft + new Vector3(doorWidth, 0),
+            lowerLeft + new Vector3(0, doorHeight),
+            lowerLeft + new Vector3(doorWidth, doorHeight)
+        };
+        vertices = vertices.Concat(newVertices).ToArray();
+        return new Face(new int[]{vertCount + 1, vertCount, vertCount + 3, vertCount, vertCount + 2, vertCount + 3});
     }
 
     /// <summary>
@@ -97,15 +153,19 @@ public class BuildingGenerator : MonoBehaviour
     /// <returns></returns>
     private List<Face> CreateWindows(ref Vector3[] vertices)
     {
+        //Remaining space should be the same on both ends
+        int horizontalWindows = (int)Math.Floor(width / (windowWidth + windowWidthSpacing * 2));
+        var relativeLeftStartPos = (width - horizontalWindows * (windowWidth + windowWidthSpacing * 2)) / 2;
+
         var createdFaces = new List<Face>();
         
         //Get the height parts of all the lower-left coordinates, goes from the height of the door section to the top
-        for (float heightLowerStart = doorSectionHeight; heightLowerStart + windowHeight < height; heightLowerStart += windowHeight + windowHeightSpacing * 2)
+        for (float heightLowerStart = doorSectionHeight; heightLowerStart + windowHeight + windowHeightSpacing < height; heightLowerStart += windowHeight + windowHeightSpacing * 2)
         {
             //Get the width parts of all the lower-left coordinates, goes from left edge + spacing to the other edge
-            for (float widthLeftStart = genCoords.x + windowWidthSpacing * 2; widthLeftStart + windowWidth < width; widthLeftStart += windowWidth + windowWidthSpacing * 2)
+            for (float widthLeftStart = genCoords.x + windowWidthSpacing + relativeLeftStartPos; widthLeftStart + windowWidth < width; widthLeftStart += windowWidth + windowWidthSpacing * 2)
             {
-                createdFaces.Add(CreateWindowAt(ref vertices, new Vector3(widthLeftStart, heightLowerStart)));
+                createdFaces.Add(CreateWindowAt(ref vertices, new Vector3(widthLeftStart, heightLowerStart, genCoords.z)));
             }
         }
 
